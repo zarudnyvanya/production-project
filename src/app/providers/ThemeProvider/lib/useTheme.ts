69369a8f88444002bba5411a48cf7d980a8882ea
Ivan <quro4ka@gmail.com>
2024-11-02 17:28:42 +0300

    feat: add blue theme

--- src/app/providers/ThemeProvider/lib/useTheme.ts
+++ src/app/providers/ThemeProvider/lib/useTheme.ts
@@ -10,7 +10,22 @@ export const useTheme = (): UseThemeResult => {
   const { theme, setTheme } = useContext(ThemeContext)
 
   const toggleTheme = () => {
-    const newTheme = theme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT
+    let newTheme: Theme
+    // theme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT
+    switch (theme) {
+      case Theme.LIGHT:
+        newTheme = Theme.DARK
+        break
+      case Theme.DARK:
+        newTheme = Theme.BLUE
+        break
+      case Theme.BLUE:
+        newTheme = Theme.LIGHT
+        break
+      default:
+        newTheme = Theme.LIGHT
+    }
+
     setTheme?.(newTheme)
     localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme)
   }
