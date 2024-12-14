05c8093bc96a2d7510a23226f65c5f1bf0ffe4e7
Ivan <quro4ka@gmail.com>
2024-12-14 19:35:49 +0300

    feat: set dotenv plugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,6 +1,7 @@
 import HtmlWebpackPlugin from 'html-webpack-plugin'
 import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 import CopyPlugin from 'copy-webpack-plugin'
+import dotenv from 'dotenv'
 import { ProgressPlugin, DefinePlugin, WebpackPluginInstance } from 'webpack'
 import { BuildOptions } from './types/config'
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
@@ -24,6 +25,7 @@ export const buildPlugins = ({
       __IS_DEV__: JSON.stringify(isDev),
       __API__: JSON.stringify(apiUrl),
       __PROJECT__: JSON.stringify(project),
+      'process.env': JSON.stringify(dotenv.config().parsed),
     }),
     new CopyPlugin({
       patterns: [{ from: paths.locales, to: paths.buildLocales }],
